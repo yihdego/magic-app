f725ea11156e930aba78fc1510e72f2af911b987
Amanuel <yihdego@msn.com>
2017-10-02 13:09:03 -0700

    added csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-CSV.foreach('db/DTK.csv', headers: true, header_converters: :symbol) do |row|
-  magic_card_info = row.to_h
-  Card.create(magic_card_info)
-end
+  CSV.foreach('db/DTK.csv', headers: true, header_converters: :symbol) do |row|
+    magic_card_info = row.to_h
+    Card.create(magic_card_info)
+  end
