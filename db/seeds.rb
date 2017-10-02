5ee8cafd2d8bbde5959e3f621d730230e5ed2619
Amanuel <yihdego@msn.com>
2017-10-02 15:25:59 -0700

    able to parse native csv file with convert file of type => card type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@ require 'csv'
   set_name = File.basename('db/DTK.csv'.downcase, ".csv")
   p set_name
   set = MagicSet.create(name: set_name)
-  CSV.foreach('db/DTK.csv', headers: true, header_converters: :symbol) do |row|
+  convert = {"name" => "name", "mana_cost" => "mana_cost", "cmc" => "cmc", "color_identity" => "color_identity", "artist" => "artist","number" => "number", "type" => "cardtype", "text" => "text", "printings" => "printings", "flavor" => "flavor", "layout" => "layout", "multiverseid" => "multiverseid", "power" => "power", "toughness" => "toughness", "rarity" => "rarity", "subtypes" => "subtypes", "types" => "types"}
+  CSV.foreach('db/DTK.csv', headers: true, header_converters: lambda { |name| convert[name] }) do |row|
     magic_card_info = row.to_h
     card = Card.create(magic_card_info)
     set.cards << card
