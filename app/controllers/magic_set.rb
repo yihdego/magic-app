c59072005849c801b30f24f4812de9bc67fedee0
Amanuel <yihdego@msn.com>
2017-10-02 16:31:35 -0700

    index and show routes for set

--- app/controllers/magic_set.rb
+++ app/controllers/magic_set.rb
@@ -1,15 +1,15 @@
 get '/magic_set' do
-  @magic_sets = Magic_set.all
-  erb :'/magic_set/index'
+  @magic_sets = MagicSet.all
+  erb :'/magic_sets/index'
 end
 
-get '/cards/dtk/:id' do
+get '/magic_set/:id' do
   p params
-  @card = Card.find(params[:id])
-  p @card
-  if request.xhr?
-    erb :'/cards/dtk/_card-details', layout: false
-  else
-    erb :'/cards/dtk/show'
-  end
+  @magic_set = MagicSet.find(params[:id])
+  p @magic_set
+  # if request.xhr?
+  #   erb :'/cards/dtk/_card-details', layout: false
+  # else
+    erb :'/magic_sets/show'
+  # end
 end
