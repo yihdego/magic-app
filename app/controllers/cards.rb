5dcb2ffaa1e872c0c84cc2bc0960a28a5214f6ff
Amanuel <yihdego@msn.com>
2017-10-02 19:24:20 -0700

    changed routes to match convention, replaced all card ids with card names

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -8,15 +8,15 @@ post '/cards/dtk' do
   redirect '/cards/dtk'
 end
 
-get '/cards/:id' do
-  @card = Card.find(params[:id])
-  erb :'/cards/show'
-end
+# get '/cards/:id' do
+#   @card = Card.find(params[:id])
+#   erb :'/cards/show'
+# end
 
 #show
-get '/magic_sets/:id/cards/:id' do
+get '/cards/:id' do
   p params
-  @card = Card.find(params[:id])
+  @card = Card.find_by(name: (params[:id].gsub("_"," ")))
   p @card
   # if request.xhr?
   #   erb :'/cards/_card-details', layout: false
