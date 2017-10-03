0abec4bf40f36a501f9023936e0cd1450aa14ca6
Amanuel <yihdego@msn.com>
2017-10-03 09:43:56 -0700

    pass user attributes test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,27 @@ class User < ActiveRecord::Base
   has_many :decks
 
   def password
-     @password ||= Password.new(password_hash)
-   end
+    @password ||= Password.new(password_hash)
+  end
 
-   def password=(new_password)
-     @password = Password.create(new_password)
-     self.password_hash = @password
-   end
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
-   def create
-     @user = User.new(params[:user])
-     @user.password = params[:password]
-     @user.save!
-   end
+  def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
+  end
 
-   def login
-     @user = User.find_by_email(params[:email])
-     if @user.password == params[:password]
-       give_token
-     else
-       redirect_to home_url
-     end
-   end
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      @user
+    else
+      nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../spec_helper'
 
 describe User do
-  let(:user) { User.new(name: "Bobby", email: 'bobby@mail.com', password: "bobby") }
+  let(:user) { User.new(name: "Adam", email: 'adamcole@mail.com', password: "Cole") }
   describe 'attributes' do
-    it 'has a name'
+    it 'has a name' do
+      expect(user.name).to eq 'Adam'
+    end
+    it 'has a email' do
+      expect(user.email).to eq 'adamcole@mail.com'
+    end
+    it 'has a password_hash' do
+      expect(user.password_hash).to_not be_nil 
+    end
   end
 end
