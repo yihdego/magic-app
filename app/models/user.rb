9217cefb85f84ebbe8d7c40a8603dc75e82ec68e
Amanuel <yihdego@msn.com>
2017-10-03 12:02:28 -0700

    added validations to user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   include BCrypt
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_hash, presence: true
   has_many :decks
 
   def password
