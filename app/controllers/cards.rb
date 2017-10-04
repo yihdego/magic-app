f0e014a2114036276353e10dcb55a16221d63e43
Amanuel <yihdego@msn.com>
2017-10-04 13:25:05 -0700

    using hassan's awesome advice .remove on card show during set reveal page

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -17,8 +17,12 @@ end
 get '/cards/:id' do
   p params
   if params[:card_name]
-    card_name = params[:card_name].gsub("_"," ")
-    @card = Card.find_by(name: card_name)
+    if params[:card_name].length > 1
+      card_name = params[:card_name].gsub("_"," ")
+      @card = Card.find_by(name: card_name)
+    else
+      redirect '/cards'
+    end
   else
     @card = Card.find_by(name: (params[:id].gsub("_"," ")))
   end
--- public/js/application.js
+++ public/js/application.js
@@ -5,10 +5,20 @@ $(document).ready(function () {
     const url = $clicked.attr("href");
     $.ajax({
       url
-
     }).done((response) => {
+      $(".card-show").remove()
       $clicked.closest('div').append(response)
     })
-
   })
+
+  // $('.card-search').on('submit', 'button' function (event){
+  //   event.preventDefault();
+  // //   const $clicked = $(event.currentTarget)
+  // //   const url = $clicked.attr("href");
+  // //   $.ajax({
+  // //     url
+  // //   }).done((response) => {
+  // //     $clicked.closest('div').append(response)
+  // //   })
+  // // })
 });
