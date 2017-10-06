1b7db8ab78744b8a90923277de5e5d09611fca81
Amanuel <yihdego@msn.com>
2017-10-06 09:42:51 -0700

    working on seeding database with decklists and adding css and javascript to nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,22 @@ Dir.glob('db/csv/*.csv') do |set|
     magicset.cards << card
   end
 end
+
+yihdego = User.create(name: "yihdego", email: "amanuel.email.com", password: "password")
+
+Dir.glob('db/decklists/*.txt') do |decklists|
+
+  deck_name = File.basename(decklists, ".txt")
+  deck = Deck.create(name: deck_name, user: yihdego)
+  File.open(decklists, "r").each_line do |line|
+    quantity_n_card_name = line.split(' ')
+    if quantity_n_card_name.length == 2
+      card = Card.find_by(name: quantity_n_card_name[1])
+      deck.add_decklist(card, quantity_n_card_name[0])
+    else
+      card = line[1..-1]
+      card = Card.find_by(name: card)
+      deck.add_decklist(card, quantity_n_card_name[0])
+    end
+  end
+end
