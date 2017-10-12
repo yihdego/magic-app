dd0c0eceb9f6dd588eb086a8c2cc8994cc761ac1
Amanuel <yihdego@msn.com>
2017-10-12 15:34:06 -0700

    added images to my site

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,4 +6,8 @@ class Card < ActiveRecord::Base
   def set_id
     self.magic_set.cards.index(self) + 1
   end
+
+  def image
+    "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{self.multiverseid}&type=card"
+  end
 end
